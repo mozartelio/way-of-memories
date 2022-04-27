a772b39e02870188f791d3edfce24f1abbdaa279
Anton Mozenbakh <anton.mozenbakh@student.tuke.sk>
2022-04-27 21:07:46 +0200

    start of level 1

--- hackathon-2022/game_hackathon/scene/level_1.gd
+++ hackathon-2022/game_hackathon/scene/level_1.gd
@@ -3,4 +3,10 @@ extends Node2D
 
 
 func _on_Player_hearth_signal(number_of_hearth):
-	print(number_of_hearth)
+	#print(number_of_hearth)
+	pass
+	
+#go out from the current scene
+func _unhandled_key_input(event):
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
