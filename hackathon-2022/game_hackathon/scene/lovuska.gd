f72e9cfa71a639d75b87157f7f7aa1f654bcb57f
Dmytro <dmytro27kagirov@gmail.com>
2022-04-29 00:35:21 +0200

    shipi

--- hackathon-2022/game_hackathon/scene/lovuska.gd
+++ hackathon-2022/game_hackathon/scene/lovuska.gd
@@ -5,4 +5,4 @@ var rotationSpeed = -0.01
 
 func _physics_process(delta):
 	$Sprite39.rotate(rotationSpeed)
-	$CollisionPolygon2D.rotate(rotationSpeed)
+	$Area2D/CollisionPolygon2D.rotate(rotationSpeed)
