9653cf129f2277913a2cec37d916f51f8d1a3002
Dmytro <dmytro27kagirov@gmail.com>
2022-04-29 01:51:17 +0200

    fix

--- hackathon-2022/game_hackathon/scene/Sprite39.gd
+++ hackathon-2022/game_hackathon/scene/Sprite39.gd
@@ -1,16 +1,9 @@
 extends Sprite
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var rotationSpeed = -0.01
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	$".".rotate(rotationSpeed)
+	$Area2D/CollisionPolygon2D.rotate(rotationSpeed)
--- hackathon-2022/game_hackathon/scene/lovuska.gd
+++ hackathon-2022/game_hackathon/scene/lovuska.gd
@@ -7,7 +7,7 @@ signal trap
 
 func _physics_process(delta):
 	$Sprite39.rotate(rotationSpeed)
-	$Area2D/CollisionPolygon2D.rotate(rotationSpeed)
+	$Sprite39/Area2D/CollisionPolygon2D.rotate(rotationSpeed)
 
 
 func _on_Area2D_body_entered(body):
